533633bdfe3db889a3a73e3ce6a7e6513ecccd6e
abisummers <hello@abisummers.com>
2018-08-21 12:00:23 +0200

    Multiple arrows

--- js/script.js
+++ js/script.js
@@ -15,62 +15,67 @@ startButton.onclick = function() {
   startButton.style.visibility = "hidden";
   match.style.visibility = "visible";
   drawScene();
-
-  jQuery(function($) {
-    $("#match").click(function(e) {
-      console.log("clicked on div");
-      e.stopPropagation(); // Prevent bubbling
-    });
-    $("body").click(function(e) {
-      console.log("clicked outside of div");
-    });
-  });
 };
 
-levelOne.onclick = function() {
-  levelOne.style.visibility = "hidden";
-  match.style.visibility = "visible";
-};
+// levelOne.onclick = function() {
+//   levelOne.style.visibility = "hidden";
+//   match.style.visibility = "visible";
+// };
 
-function Arrow(direction) {
+function Arrow(direction, myArrowWidth) {
   this.img = new Image();
   this.img.src = `./images/${direction}-arrow.png`;
+  this.arrowHeight = 160;
+  this.arrowWidth = myArrowWidth;
+  this.x = 100;
+  this.y = 100;
+
   this.drawMe = () => {
-    ctx.drawImage(this.img, arrowWidth, arrowHeight, 100, 100);
+    ctx.drawImage(this.img, this.arrowWidth, this.arrowHeight, this.x, this.y);
   };
 }
 
 var allArrows = [];
-var down = new Arrow("down");
+var down = new Arrow("down", 1500);
 allArrows.push(down);
 
-var up = new Arrow("up");
+var up = new Arrow("up", 1800);
 allArrows.push(up);
 
-var left = new Arrow("left");
+var left = new Arrow("left", 2000);
 allArrows.push(left);
 
-var right = new Arrow("right");
+var right = new Arrow("right", 2200);
 allArrows.push(right);
 
-var arrowHeight = 160;
-var arrowWidth = 1500;
+// var arrowHeight = 160;
+// var arrowWidth = 1500;
 
 console.log(allArrows);
 
 function drawScene() {
   ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
-  arrowWidth -= 4;
-  ctx.drawImage(down.img, arrowWidth, arrowHeight, 100, 100);
-  // allArrows.forEach(function(el) {
-  //   el.drawMe();
-  // });
+  //ctx.drawImage(down.img, arrowWidth, arrowHeight, 100, 100);
+  allArrows.forEach(function(el) {
+    // setInterval(function() {
+    el.arrowWidth -= 2;
+    el.drawMe();
+    // }, 1000);
+  });
 
   requestAnimationFrame(function() {
     drawScene();
   });
 }
 
+// img.onload = drawScene;
+
 var matchCoord = match.getBoundingClientRect();
 var leftX = matchCoord.left;
 var rightX = matchCoord.right;
+
+jQuery(function($) {
+  $("#match").click(function(e) {
+    console.log("clicked on div");
+  });
+});
